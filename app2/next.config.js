1595cb3a53d9ce4d2c882f9c524cca02b456db7b
Jens Fischer <jens@weareprofound.com>
2019-05-08 18:23:15 -0400

    initial setup

---
+++ app2/next.config.js
@@ -0,0 +1,4 @@
+module.exports = () => ({
+  distDir: '../dist',
+  useFileSystemPublicRoutes: true
+});
---
+++ app3/src/pages/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    <h1>EXAMPLE APP 3</h1>
+    <h2>Basepath:</h2>
+    <p>/app3</p>
+    <h2>Routes:</h2>
+    <ul>
+      <li><a href="/">/ (CURRENT)</a></li>
+    </ul>
+    <h2>Static folder:</h2>
+    <p>/static</p>
+    <img src="/static/image.png" />
+    <h2>Total Apps:</h2>
+    <ul>
+      <li><a href="/">App 1</a></li>
+      <li><a href="/app2">App 2</a></li>
+      <li><a href="/app3">App 3 (CURRENT)</a></li>
+    </ul>
+  </div>
+);
